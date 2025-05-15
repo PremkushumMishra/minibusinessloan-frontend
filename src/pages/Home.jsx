970c4fb694c479f3c4c3e7baeed5b9afd40b818a
prem kushum <premkushum2@gmail.com>
2025-05-15 14:51:55 +0530

    change sanction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
       <AppDownload/>
       {/* <BsaRejectes/> */}
       <PublicReview />
-      <SanctionLatter/>
+      {/* <SanctionLatter/> */}
       {/* <GetAmount /> */}
       {/* <KycSuccess />/ */}
       {/* <CoApplicant/> */}
